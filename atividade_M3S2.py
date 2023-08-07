08f8b52ebbff5a44300864e58a32c7f5d5558f59
ErnestoFortes <ernestofortes@gmail.com>
2023-08-07 13:59:33 -0300

    Atividade do modulo 3 da semana 2 inciada

---
+++ atividade_M3S2.py
@@ -0,0 +1,21 @@
+
+'''
+Uma pessoa do seu time de desenvolvimento está escrevendo várias funções que calculam diferentes
+formas de gerar juros. você deve escrever um decorator chamado decorator_imprimir, que será usado para a função chamada imprima os parâmetros:
+capital, taxa e tempo, além do resultado da função.
+'''
+def decorador_imprimir(funcao):
+    def dados(*args):
+        dados_entrada = f'CAPITAL: {args[0]} TAXA: {args[1]} TEMPO: {args[2]}'
+        calc_juro = funcao(*args)
+        resultado= f'RESULTADO:  {calc_juro}'
+        print(f'\n{dados_entrada} \n{resultado}')
+    return dados
+
+
+@decorador_imprimir
+def juros_simples(capital, taxa, tempo):
+    return capital * (taxa/100) * tempo
+
+
+juros_simples(1000, 5, 6)
